7c7e122fcd8fd64ddb1f25250f1997dcb3f64a61
AnkanHalder <halderankan95@gmail.com>
2023-04-12 12:15:05 +0530

    Initial Draft of NewFlix Backend

--- NewFlixBackendServer.js
+++ NewFlixBackendServer.js
@@ -3,92 +3,47 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const  schemas = require("./schemas.js");
+const dbClass = require('./dbClass.js');
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  ph_number: Number,
-  email: String,
-  password: String,
-  Login: Boolean,
-  watchlist: Array,
-  liked: Array,
-  last_viewed: Array,
-  catagories: Array,
-});
-mongoose.connect("mongodb://localhost:27017/userDataDB",{useNewUrlParser:true})
-// mongoose.connect(
-//   "mongodb+srv://" +
-//     process.env.mongoEmailID +
-//     ":" +
-//     process.env.mongoPassword +
-//     "@cluster0.bg35g.mongodb.net/userDataDB"
-// );
-const person = mongoose.model("userData", userSchema);
+// const userSchema = schemas.userSchema;
+// const videoF = schemas.videoF;
+// //mongoose.connect("mongodb://localhost:27017/userDataDB",{useNewUrlParser:true})
+// mongoose.connect("mongodb+srv://" + process.env.mongoEmailID+ ":" + process.env.mongoPassword + "@cluster0.bg35g.mongodb.net/userDataDB");
+// const person = mongoose.model("userData", userSchema);
+// const video = mongoose.model("videoData", videoF);
+
+const db  = new dbClass();
+console.log(db);
 
 app.route("/register/:name/:email/:password").get((req, res) =>{
-  person.findOne({ email: req.params.email }, function (err, personData){
-    let data = {
-      email: false,
-      reg:false,
-      error:""
-    };
-      if(err)  
-      {
-        console.log(err) ;
-        data[error]=err;
-        res.send(data);
-      }   
-      if(personData==null){
-        const newUser=new person({
-          name:req.params.name,
-          email:req.params.email,
-          password:req.params.password
-      })
-      newUser.save(function(err){
-        if(err) {res.send("Registration unsuccessful")}
-        else {res.send("Registration successful")}
-      })
-      data[reg]=true;
-      res.send(data);
-    }
-    else{
-      data[email]=true;
-      res.send(data);
-    }
+      db.Registration(req.params.name, req.params.email,req.params.password,res);
   })
-})
+
+
+
 app.route("/login/:email/:password").get((req, res) => {
   //res.render("Hello")
-  let data = {
-    email: false,
-    password: false,
-    signedIn: false,
-  };
-  person.findOne({ email: req.params.email }, function (err, personData) {
-    if (err) console.log(err);
-    //res.send(data);
-    if (personData == null){
-        newUser.save(function(err){
-          if(err) {res.send("Registration unsuccessful")}
-          else {res.send("Registration successful")}
-        })
-    }             
-    else {
-      data.email = true;
-      if (personData.password == req.params.password) {
-        data.password = true;
-        data.signedIn = true;
-      } //s += "Error! User Already logged in!<br>";}
-      res.send(data);
-    }
-  });
-});
+      db.Login(req.params.email,req.params.password,res);
+    });
+
+
+app.route("/main/getRow/:catagory/:limit").get(function(req,res){
+        db.SearchByCatagory(req.params.catagory,req.params.limit,res);
+      });
+app.route("/playVideo/:id").get(function(req,res){
+        db.SearchById(req.params.id,res);
+            });
+
+
+
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
 
-app.listen(port, () => {
+app.listen(8000, () => {
   console.log("Server is Running ...... ");
 });
